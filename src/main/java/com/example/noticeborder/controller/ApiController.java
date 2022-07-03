dc26906894b8f06058afcfc3f011d2db41b3e9bc
ekslffh <ekslffh@naver.com>
2022-07-04 02:04:43 +0900

    Create service and controller and complete testing through Swagger-ui

--- src/main/java/com/example/noticeborder/controller/ApiController.java
+++ src/main/java/com/example/noticeborder/controller/ApiController.java
@@ -1,14 +1,43 @@
 package com.example.noticeborder.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.noticeborder.entity.PostEntity;
+import com.example.noticeborder.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
     }
+
+    @PostMapping
+    public void post(@RequestBody PostEntity postEntity) {
+        boardService.save(postEntity);
+        System.out.println(postEntity);
+    }
+
+    @GetMapping
+    public List<PostEntity> findAll() {
+        return boardService.findAll();
+    }
+
+    @PutMapping
+    public void put(@RequestBody PostEntity postEntity) {
+        boardService.save(postEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        boardService.delete(id);
+    }
+
 }
